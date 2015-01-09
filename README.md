df7732b8b0c4189ddd02cb1d25bac1e9cecc2bf5
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2015-01-09 22:55:29 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# binge.sh
+or "oh god I spent an hour writing this instead of watching tv shows."
+
+(early versions were known as "did this just delete half of my folder?")
+
+## what does it do
+binge.sh is a short bash script which keeps track of your progress through a folder of tv shows.
+
+## usage
+
+`./binge.sh` binge watch the current folder
+
+`./binge.sh -s [path/to/series]` set the default tv show to binge
+
+`./binge.sh -d` binge watch the default tv show
+
+## license
+this work is dedicated to the public domain
---
+++ binge.sh
@@ -0,0 +1,79 @@
+#! /bin/bash
+# -s directory: set default
+# -d use default
+# http://xkcd.com/1319/
+
+if [ "$1" == "-h" ]; then
+	echo "binge [-d] [-s directory]"
+	exit
+elif [ "$1" == "-s" ]; then
+	echo $2 > ~/.current_binge
+fi
+
+if [ "$1" == "-d" ] && [ -e ~/.current_binge ]; then
+	cd $(cat ~/.current_binge)
+fi
+
+if [ -e .current_episode ]; then
+	found_episode=false
+else
+	echo -n "init? (y/n) "
+	read op
+	while true; do
+		if [ $op = "y" ]; then
+			found_episode=true
+			break
+		elif [ $op = "n" ]; then
+			exit
+		fi
+	done
+fi
+
+if [ -e .last_timestamp ]; then
+	echo "Last episode stopped at $(cat .last_timestamp)"
+	rm .last_timestamp
+fi
+
+for ep_count in $(seq 1 $(ls | wc -l)); do
+	ep=`ls | head -n $ep_count | tail -n 1`
+
+	if ! $found_episode; then
+		current_episode=`cat .current_episode`
+		if [ "$current_episode" = "$ep" ]; then
+			found_episode=true
+		else
+			continue
+		fi
+	fi
+		
+	echo $ep > .current_episode
+	
+	while true; do
+		echo -n "watch \"$ep\"? (y/n/+) "
+		
+		read op
+		
+		if [ $op = "y" ]; then
+			open "$ep"
+			
+			while true; do
+				echo -n "finished? (y/n) "
+				read op
+				if [ $op = "y" ]; then
+					continue 3
+				elif [ $op = "n" ]; then
+					echo -n "current time? "
+					read ep_time
+					echo $ep_time > .last_timestamp
+					break 3
+				fi
+			done
+		else
+			if [ $op = "+" ]; then
+				continue 2
+			elif [ $op = "n" ]; then
+				break 2
+			fi
+		fi
+	done
+done
